7a8a50dfd243f98c7e8b319166dd38288b62efa6
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-25 05:02:20 +0300

    feat: added functionality to create and save new user

--- src/main/java/com/willis/trading/controller/AuthController.java
+++ src/main/java/com/willis/trading/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.willis.trading.controller;
 
+import com.willis.trading.model.User;
 import com.willis.trading.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     @Autowired
     private UserRepository userRepository;
+
+    public ResponseEntity<User> register(User user){
+        User newUser = new User();
+        newUser.setEmail(user.getEmail());
+        newUser.setFullName(user.getFullName());
+        newUser.setPassword(user.getPassword());
+
+        User savedUser = userRepository.save(newUser);
+
+        
+
+    }
 }
