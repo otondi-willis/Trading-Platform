31ba3f51d8a262ccc911cbf8f6fb9ef8f89ba7a3
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-25 04:10:47 +0300

    First commit

---
+++ src/test/java/com/willis/trading/TradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willis.trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
