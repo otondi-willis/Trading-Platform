53f80079bfbecb785ee102a12eeeb132f4e108f6
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-25 04:36:05 +0300

    feat: (model)automatic user id generation

---
+++ src/main/java/com/willis/trading/model/User.java
@@ -0,0 +1,15 @@
+package com.willis.trading.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}
