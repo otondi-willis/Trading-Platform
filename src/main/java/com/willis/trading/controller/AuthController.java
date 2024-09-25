264871e2859b29653e1a338c5a8f456b52f8605e
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-25 04:55:24 +0300

    feat: created auth controller

---
+++ src/main/java/com/willis/trading/controller/AuthController.java
@@ -0,0 +1,13 @@
+package com.willis.trading.controller;
+
+import com.willis.trading.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private UserRepository userRepository;
+}
