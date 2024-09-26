221f1ca2bbfe991b0de2f357e9f6257e5362cf40
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-26 05:03:44 +0300

    feat: added JwtConstant to constitute jwt header value and security key value

--- src/main/java/com/willis/trading/config/JwtConstant.java
+++ src/main/java/com/willis/trading/config/JwtConstant.java
@@ -1,5 +1,6 @@
 package com.willis.trading.config;
 
 public class JwtConstant {
-
+    public static final String SECRET_KEY="ldjfajfaiojedfcvkjoaiejdvmcolkjgaodjmv;cljgaoijgdvkcmxagosedjoje";
+    public static final String JWT_HEADER="Authorization";
 }
