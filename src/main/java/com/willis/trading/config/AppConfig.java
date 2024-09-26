2eb897a7e3b6ae4b8dbcc79956d3000617303af6
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-26 04:54:58 +0300

    feat: added app configuration

---
+++ src/main/java/com/willis/trading/config/AppConfig.java
@@ -0,0 +1,25 @@
+package com.willis.trading.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@Configuration
+public class AppConfig {
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.sessionManagement(management->management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(Authorize->Authorize.requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll())
+                .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+                .csrf(csrf->csrf.disable())
+                .cors(cors-> cors.configurationSource(corsConfigurationSource()))
+        return null;
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        return null;
+    }
+}
