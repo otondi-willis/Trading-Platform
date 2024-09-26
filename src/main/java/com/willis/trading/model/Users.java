3fa0382caff878ccdff19571dcad5c89252f0e30
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-26 04:11:42 +0300

    feat: class name refactoring correct functioning of Hibernate

--- src/main/java/com/willis/trading/model/Users.java
+++ src/main/java/com/willis/trading/model/Users.java
@@ -2,25 +2,24 @@ package com.willis.trading.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.willis.trading.domain.USER_ROLE;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
-public class User {
+public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String fullName;
     private String email;
+    private int mobile;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
+    @Embedded
     private TwoFactorAuth twoFactorAuth=new TwoFactorAuth();
 
     private USER_ROLE role= USER_ROLE.ROLE_CUSTOMER;
