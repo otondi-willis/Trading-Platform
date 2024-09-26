41f16dd76bac8f9d086b0b7c048973bc1eefe53d
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-26 04:11:57 +0300

    feat: class name refactoring correct functioning of Hibernate

--- src/main/java/com/willis/trading/repository/UserRepository.java
+++ src/main/java/com/willis/trading/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.willis.trading.repository;
 
-import com.willis.trading.model.User;
+import com.willis.trading.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<Users,Long> {
 }
