a4849832cc11260edfa79a9e0bb088f071078080
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-26 04:58:01 +0300

    feat: added JwtTokenValidator

---
+++ src/main/java/com/willis/trading/config/JwtTokenValidator.java
@@ -0,0 +1,17 @@
+package com.willis.trading.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtTokenValidator extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String jwt=request.getHeader("");
+    }
+}
