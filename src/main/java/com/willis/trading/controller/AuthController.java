d74fb1e5b6705839c121eb38dde5008437251824
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-26 03:24:10 +0300

    feat: return entity with created http status code

--- src/main/java/com/willis/trading/controller/AuthController.java
+++ src/main/java/com/willis/trading/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.willis.trading.controller;
 import com.willis.trading.model.User;
 import com.willis.trading.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ public class AuthController {
         newUser.setPassword(user.getPassword());
 
         User savedUser = userRepository.save(newUser);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
 
         
 
