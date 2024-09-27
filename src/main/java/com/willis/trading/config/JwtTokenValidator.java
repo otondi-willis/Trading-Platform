feb7cd83b1d04e7c44da5fee2b30c3fafc84eb86
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-27 03:35:45 +0300

    feat: adding functionality of jwt method

--- src/main/java/com/willis/trading/config/JwtTokenValidator.java
+++ src/main/java/com/willis/trading/config/JwtTokenValidator.java
@@ -1,14 +1,23 @@
 package com.willis.trading.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.crypto.SecretKey;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JwtTokenValidator extends OncePerRequestFilter {
 
@@ -22,6 +31,19 @@ public class JwtTokenValidator extends OncePerRequestFilter {
 
             try {
                 SecretKey key = Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
+
+                Claims claims= Jwts.parser().setSigningKey(key).build().parseClaimsJwt(jwt).getBody();
+                String email= String.valueOf(claims.get("email"));
+
+                String authorities=String.valueOf(claims.get("authorities"));
+                List<GrantedAuthority> authoritiesList= AuthorityUtils.commaSeparatedStringToAuthorityList(authorities);
+                Authentication auth=new UsernamePasswordAuthenticationToken(
+                        email,
+                        authoritiesList,
+                        authoritiesList
+                );
+
+                SecurityContextHolder.getContext().setAuthentication(auth);
 
             }catch(Exception e){
                 throw new RuntimeException("invalid token...");
