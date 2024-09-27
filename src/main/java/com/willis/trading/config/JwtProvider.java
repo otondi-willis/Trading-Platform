ad0514f2401613b3c7167df9e0fbf62b910679a0
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-27 04:52:10 +0300

    feat: created jwt provider

--- src/main/java/com/willis/trading/config/JwtProvider.java
+++ src/main/java/com/willis/trading/config/JwtProvider.java
@@ -1,11 +1,41 @@
 package com.willis.trading.config;
 
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 
 import javax.crypto.SecretKey;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 public class JwtProvider {
 
     private static SecretKey key =Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
+
+    public static String generateToken(Authentication auth){
+        Collections<? extends GrantedAuthority> authorities = auth.getAuthorities();
+        String roles=popupateAuthorities(authorities);
+
+        String jwt= Jwts.builder()
+                .setIssuedAt(new Date())
+                .setExpiration( new Date(new Date().getTime()+86400000))
+                .claim("email",auth.getName())
+                .claim("authorities",roles)
+                .signWith(key)
+                .compact();
+        return jwt;
+
+    }
+
+    private static String popupateAuthorities(Collections authorities) {
+        Set<String> auth=new HashSet<>();
+        for(GrantedAuthority ga:authorities){
+            auth.add(ga.getAuthority());
+        }
+        return String.join(",",auth);
+    }
 
 }
