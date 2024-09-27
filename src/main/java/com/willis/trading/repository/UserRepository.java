e5ac86a936faf86a8524ccbe4e95d9ba7dadcd0d
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-27 04:17:07 +0300

    feat: findByEmail method added

--- src/main/java/com/willis/trading/repository/UserRepository.java
+++ src/main/java/com/willis/trading/repository/UserRepository.java
@@ -2,6 +2,9 @@ package com.willis.trading.repository;
 
 import com.willis.trading.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
 
 public interface UserRepository extends JpaRepository<Users,Long> {
+
+    Users findByEmail(String email);
 }
