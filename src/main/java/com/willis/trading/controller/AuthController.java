368432b03029c8519e5b86b298ec5b932f698e03
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-01 03:50:12 +0300

    Controller: checking if twoFactorAuth is enabled

--- src/main/java/com/willis/trading/controller/AuthController.java
+++ src/main/java/com/willis/trading/controller/AuthController.java
@@ -4,12 +4,15 @@ import com.willis.trading.config.JwtProvider;
 import com.willis.trading.model.Users;
 import com.willis.trading.repository.UserRepository;
 import com.willis.trading.response.AuthResponse;
+import com.willis.trading.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +24,8 @@ public class AuthController {
     @Autowired
     private UserRepository userRepository;
 
-
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
 
     @PostMapping("/register")
     public ResponseEntity<AuthResponse> register(@RequestBody Users users) throws Exception {
@@ -60,5 +64,45 @@ public class AuthController {
 
         
 
+    }
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody Users users) throws Exception {
+
+       String userName=users.getEmail();
+       String password=users.getPassword();
+
+
+        Authentication auth=authenticateM(userName,password);
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        String jwt= JwtProvider.generateToken(auth);
+
+        if(users.getTwoFactorAuth().isEnabled()){
+
+        }
+
+        AuthResponse res=new AuthResponse();
+        res.setJwt(jwt);
+        res.setStatus(true);
+        res.setMessage("login successful");
+
+
+        return new ResponseEntity<>(res, HttpStatus.CREATED);
+
+
+
+    }
+
+    private Authentication authenticateM(String userName, String password) {
+        UserDetails userDetails=customUserDetailsService.loadUserByUsername(userName);
+        if(userDetails==null){
+            throw new BadCredentialsException("invalid username");
+
+        }
+        if(!password.equals(userDetails.getPassword())){
+            throw new BadCredentialsException("invalid password");
+        }
+        return new UsernamePasswordAuthenticationToken(userDetails,password,userDetails.getAuthorities());
     }
 }
