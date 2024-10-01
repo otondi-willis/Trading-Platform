2b45505f34b16236f2da1a942c9de948e4a35072
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-01 04:12:06 +0300

    Added OtpUtils class

---
+++ src/main/java/com/willis/trading/utils/OtpUtils.java
@@ -0,0 +1,16 @@
+package com.willis.trading.utils;
+
+import java.util.Random;
+
+public class OtpUtils {
+    public static String generateOTP(){
+        int otpLength = 6;
+        Random random=new Random();
+        StringBuilder otp=new StringBuilder(otpLength);
+        for(int i=0;i<otpLength;i++){
+            otp.append(random.nextInt(10));
+
+        }
+        return otp.toString();
+    }
+}
