1343dd787c3e373598ba86cc4e33d6a29173779b
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-02 04:51:51 +0300

    feat: added endpoints

--- src/main/java/com/willis/trading/controller/AuthController.java
+++ src/main/java/com/willis/trading/controller/AuthController.java
@@ -128,6 +128,7 @@ public class AuthController {
         }
         return new UsernamePasswordAuthenticationToken(userDetails,password,userDetails.getAuthorities());
     }
+    @PostMapping("two-factor/otp/{otp}")
     public ResponseEntity<AuthResponse> verifySigninOtp(@PathVariable String otp, @RequestParam String id) throws Exception {
 
         TwoFactorOTP twoFactorOTP=twoFactorOtpService.findById(id);
--- src/main/java/com/willis/trading/controller/UserController.java
+++ src/main/java/com/willis/trading/controller/UserController.java
@@ -1,13 +1,13 @@
 package com.willis.trading.controller;
 
+import com.willis.trading.domain.VerificationType;
 import com.willis.trading.model.Users;
 import com.willis.trading.service.EmailService;
 import com.willis.trading.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -17,10 +17,29 @@ public class UserController {
     @Autowired
     private EmailService emailService;
 
+    @GetMapping("/api/users/profile")
     public ResponseEntity<Users> getUserProfile(
             @RequestHeader("Authorization") String jwt) throws Exception {
         Users user=userService.findUserProfileByJwt(jwt);
-        return new ResponseEntity<Users>(user, HttpStatus.OK)
+        return new ResponseEntity<Users>(user, HttpStatus.OK);
+    }
+
+    @PostMapping("/api/users/verification/{verificationType}/send-otp")
+    public ResponseEntity<Users> sendVerificationOtp(
+            @RequestHeader("Authorization") String jwt,
+            @PathVariable VerificationType verificationType) throws Exception {
+
+        Users user=userService.findUserProfileByJwt(jwt);
+
+
+        return new ResponseEntity<Users>(user, HttpStatus.OK);
+    }
+
+    @PatchMapping("/api/users/enable-two-factor/verify-otp/{otp}")
+    public ResponseEntity<Users> enableTwoFactorAuthentication(
+            @RequestHeader("Authorization") String jwt) throws Exception {
+        Users user=userService.findUserProfileByJwt(jwt);
+        return new ResponseEntity<Users>(user, HttpStatus.OK);
     }
 
 }
