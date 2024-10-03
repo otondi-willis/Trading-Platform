f20faa6291bc5bc42dac6c847493274cfa269256
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-03 03:21:37 +0300

    feat: injected VerificationCodeRepository

--- src/main/java/com/willis/trading/service/VerificationCodeServiceImpl.java
+++ src/main/java/com/willis/trading/service/VerificationCodeServiceImpl.java
@@ -1,8 +1,14 @@
 package com.willis.trading.service;
 
 import com.willis.trading.model.VerificationCode;
+import com.willis.trading.repository.VerificationCodeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class VerificationCodeServiceImpl implements VerificationCodeService {
+    @Autowired
+    private VerificationCodeRepository verificationCodeRepository;
     @Override
     public VerificationCode sendVerificationCode(VerificationCode verificationCode) {
         return null;
