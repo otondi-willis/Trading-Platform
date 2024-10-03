b87a9b010deba4fd2973d431d05296062c324f8f
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-03 03:29:35 +0300

    feat: added implementatioon to  sendVerificationCode method

--- src/main/java/com/willis/trading/service/VerificationCodeServiceImpl.java
+++ src/main/java/com/willis/trading/service/VerificationCodeServiceImpl.java
@@ -1,7 +1,10 @@
 package com.willis.trading.service;
 
+import com.willis.trading.domain.VerificationType;
+import com.willis.trading.model.Users;
 import com.willis.trading.model.VerificationCode;
 import com.willis.trading.repository.VerificationCodeRepository;
+import com.willis.trading.utils.OtpUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +13,13 @@ public class VerificationCodeServiceImpl implements VerificationCodeService {
     @Autowired
     private VerificationCodeRepository verificationCodeRepository;
     @Override
-    public VerificationCode sendVerificationCode(VerificationCode verificationCode) {
-        return null;
+    public VerificationCode sendVerificationCode(Users user, VerificationType verificationType) {
+        VerificationCode verificationCode1=new VerificationCode();
+        verificationCode1.setOtp(OtpUtils.generateOTP());
+        verificationCode1.setVerificationType(verificationType);
+        verificationCode1.setUser(user);
+
+        return verificationCodeRepository.save(verificationCode1);
     }
 
     @Override
