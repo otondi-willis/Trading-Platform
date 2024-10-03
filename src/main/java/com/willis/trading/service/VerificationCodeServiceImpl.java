f89f9a8c93738a296bc87d981314acf0ce052422
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-03 03:18:51 +0300

    feat: added verification code service class

---
+++ src/main/java/com/willis/trading/service/VerificationCodeServiceImpl.java
@@ -0,0 +1,25 @@
+package com.willis.trading.service;
+
+import com.willis.trading.model.VerificationCode;
+
+public class VerificationCodeServiceImpl implements VerificationCodeService {
+    @Override
+    public VerificationCode sendVerificationCode(VerificationCode verificationCode) {
+        return null;
+    }
+
+    @Override
+    public VerificationCode getVerificationCodeById(Long id) {
+        return null;
+    }
+
+    @Override
+    public VerificationCode getVerificationCodeByUser(Long userId) {
+        return null;
+    }
+
+    @Override
+    public void deleteVerificatonCodeById(VerificationCode verificationCode) {
+
+    }
+}
