1da2a0a6bc3e5b4a7722345e110cd4e66561fa62
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-03 03:26:39 +0300

    feat: changed arguments of sendVerificationCode declaration

--- src/main/java/com/willis/trading/service/VerificationCodeService.java
+++ src/main/java/com/willis/trading/service/VerificationCodeService.java
@@ -1,9 +1,12 @@
 package com.willis.trading.service;
 
+import com.willis.trading.domain.VerificationType;
+import com.willis.trading.model.Users;
 import com.willis.trading.model.VerificationCode;
+import org.springframework.security.core.userdetails.User;
 
 public interface VerificationCodeService {
-    VerificationCode sendVerificationCode(VerificationCode verificationCode);
+    VerificationCode sendVerificationCode(Users user, VerificationType verificationType);
     VerificationCode getVerificationCodeById(Long id);
     VerificationCode getVerificationCodeByUser(Long userId);
     void deleteVerificatonCodeById(VerificationCode verificationCode);
