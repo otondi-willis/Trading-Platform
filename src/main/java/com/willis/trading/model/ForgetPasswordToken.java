82b95368f3cae05920825f23dab478bcac355916
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-04 03:26:59 +0300

    feat: added Forgot Password service class

---
+++ src/main/java/com/willis/trading/model/ForgetPasswordToken.java
@@ -0,0 +1,22 @@
+package com.willis.trading.model;
+
+import com.willis.trading.domain.VerificationType;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class ForgetPasswordToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    private Users user;
+
+    private String otp;
+    private VerificationType verificationType;
+    private String sendTo;
+
+
+}
