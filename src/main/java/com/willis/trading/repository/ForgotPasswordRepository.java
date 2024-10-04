07c93bd8315158db66b604a522f721e145a1b976
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-04 03:52:06 +0300

    feat:created ForgotPasswordRepository interface

---
+++ src/main/java/com/willis/trading/repository/ForgotPasswordRepository.java
@@ -0,0 +1,8 @@
+package com.willis.trading.repository;
+
+import com.willis.trading.model.ForgotPasswordToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ForgotPasswordRepository extends JpaRepository<ForgotPasswordToken,String> {
+    ForgotPasswordToken findByUserId(Long userId);
+}
